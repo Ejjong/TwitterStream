4e8fc229ee3cc819dcfd99adbe20eda7c03a6159
Jongho Lee <ejjong7@gmail.com>
2014-01-08 17:39:48 +0900

    first commit

---
+++ src/TwitterStreamClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitterStreamClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "2276962602-nAw6FGdvi0znOodwi3bKk6UA7KVMN25Sz4hLhbo";
+            string secret = "OTkcTIO1ndAIWvt2s6zPbVHrOviSSbUN9p3v6xeT2LSyu";
+            string consumerKey = "gjj9HQM8FfHKadq97K3g";
+            string consumerSecret = "9rD3gfrCT6AzOrSa0ras9dlzlbgLH6RWTGq3ry9uPc0";
+            var tsClient = new TwitterStreamClient(token, secret, consumerKey, consumerSecret);
+
+            var block = new AutoResetEvent(false);
+
+            tsClient.Start();
+
+            block.WaitOne();
+        }
+    }
+}
