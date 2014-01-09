103d11f1d7b84a8f66603fabb52ab27ccbc4c79e
Jongho Lee <ejjong7@gmail.com>
2014-01-09 17:06:45 +0900

    change twitter 3rd lib

--- src/TwitterStreamClient/Program.cs
+++ src/TwitterStreamClient/Program.cs
@@ -15,7 +15,7 @@ namespace TwitterStreamClient
             string secret = "OTkcTIO1ndAIWvt2s6zPbVHrOviSSbUN9p3v6xeT2LSyu";
             string consumerKey = "gjj9HQM8FfHKadq97K3g";
             string consumerSecret = "9rD3gfrCT6AzOrSa0ras9dlzlbgLH6RWTGq3ry9uPc0";
-            var tsClient = new TwitterStreamClient(token, secret, consumerKey, consumerSecret);
+            var tsClient = new TwitterStreamClient2(token, secret, consumerKey, consumerSecret);
 
             var block = new AutoResetEvent(false);
 
---
+++ src/TwitterStreamClient/TwitterStreamClient2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetinCore.Interfaces.TwitterToken;
+using TwitterToken;
+using Tweetinvi;
+using Streaminvi;
+
+namespace TwitterStreamClient
+{
+    public class TwitterStreamClient2
+    {
+        IToken _token;
+        public TwitterStreamClient2(string token, string secret, string consumerKey, string consumerSecret)
+        {
+            _token = new Token(token, secret, consumerKey, consumerSecret);
+        }
+
+        public void Start()
+        {
+            var stream = new UserStream(_token);
+            stream.MessageReceivedFromX += (sender, args) =>
+            {
+                Console.WriteLine("Message '{0}' received from {1}!", args.Value.Text, args.Value3.Id);
+            };
+            stream.StartStream();
+        }
+    }
+}
