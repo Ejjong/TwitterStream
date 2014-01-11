d290344f00787f184b0f4240ced33f9843523060
Jongho Lee <ejjong7@gmail.com>
2014-01-11 13:28:52 +0900

    fix sh

--- linuxDeploy.sh
+++ linuxDeploy.sh
@@ -1,3 +1,4 @@
 DEST=/home/ejjong/RiderStatus/
-rm -v ${DEST}* ! -name Status*
+cd ${DEST}
+rm -v -- "--exclude Status*"
 rsync -av --exclude Status.*  /var/TeamCity/buildAgent/work/f5a388f2ffffb4ec/output/Release/ ${DEST}
