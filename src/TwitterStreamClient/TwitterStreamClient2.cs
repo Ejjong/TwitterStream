5412317bab0b73c42d1fa088a6b843fff3a40289
Jongho Lee <ejjong7@gmail.com>
2014-01-12 14:19:16 +0900

    add try exception

--- src/TwitterStreamClient/TwitterStreamClient2.cs
+++ src/TwitterStreamClient/TwitterStreamClient2.cs
@@ -26,74 +26,85 @@ namespace TwitterStreamClient
 
         public void Start()
         {
-            Console.WriteLine("start");
-            var stream = new UserStream(_token);
-            var curPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            stream.MessageReceivedFromX += (sender, args) =>
-            {;
-                var xmlText = string.Empty;
-                Console.WriteLine("before read");
-                try
+            try
+            {
+                Console.WriteLine("start");
+                var stream = new UserStream(_token);
+                var curPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                stream.MessageReceivedFromX += (sender, args) =>
                 {
-                    xmlText = File.ReadAllText(Path.Combine(curPath,"Status.xml"));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                Console.WriteLine("after read");
-                var doc = XDocument.Parse(xmlText);
-                if (args == null || args.Value3.Id == null) return;
+                    ;
+                    var xmlText = string.Empty;
+                    Console.WriteLine("before read");
+                    try
+                    {
+                        xmlText = File.ReadAllText(Path.Combine(curPath, "Status.xml"));
+ 
+                    Console.WriteLine("after read");
+                    var doc = XDocument.Parse(xmlText);
+                    if (args == null || args.Value3.Id == null) return;
 
-                var strings = args.Value.Text.Split(',');
-                var riders = doc.Descendants("Riders").FirstOrDefault();
-                if (riders != null)
-                {
-                    var rider = riders.Descendants("Rider").SingleOrDefault(r => r.Attribute("Id").Value == args.Value3.Id.ToString());
-                    if (rider != null)
+                    var strings = args.Value.Text.Split(',');
+                    var riders = doc.Descendants("Riders").FirstOrDefault();
+                    if (riders != null)
                     {
-                        switch (strings.GetStatus())
+                        var rider =
+                            riders.Descendants("Rider")
+                                .SingleOrDefault(r => r.Attribute("Id").Value == args.Value3.Id.ToString());
+                        if (rider != null)
                         {
-                            case "Online":
-                            case "Riding":
-                                rider.SetAttributeValue("Name", args.Value3.Name);
-                                rider.SetAttributeValue("Status", strings.GetStatus());
-                                rider.SetAttributeValue("Message", strings.GetMessage());
-                                rider.SetAttributeValue("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-                                SendMessage(args.Value3.Id, strings.GetStatus(), _token);
-                                break;
-                            case "Offline":
-                                rider.Remove();
-                                SendMessage(args.Value3.Id, strings.GetStatus(), _token);
-                                break;
-                            default:
-                                if (args.Value3.Id == 229481394)
-                                    SendMessage(args.Value3.Id, "pingtest", _token);
-                                break;
+                            switch (strings.GetStatus())
+                            {
+                                case "Online":
+                                case "Riding":
+                                    rider.SetAttributeValue("Name", args.Value3.Name);
+                                    rider.SetAttributeValue("Status", strings.GetStatus());
+                                    rider.SetAttributeValue("Message", strings.GetMessage());
+                                    rider.SetAttributeValue("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                                    SendMessage(args.Value3.Id, strings.GetStatus(), _token);
+                                    break;
+                                case "Offline":
+                                    rider.Remove();
+                                    SendMessage(args.Value3.Id, strings.GetStatus(), _token);
+                                    break;
+                                default:
+                                    if (args.Value3.Id == 229481394)
+                                        SendMessage(args.Value3.Id, "pingtest", _token);
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            if (strings.GetStatus() == "Offline")
+                            {
+                                SendMessage(args.Value3.Id, "already Offline", _token);
+                                return;
+                            }
+                            riders.Add(new XElement("Rider",
+                                new XAttribute("Id", args.Value3.Id),
+                                new XAttribute("Name", args.Value3.Name),
+                                new XAttribute("Status", strings.GetStatus()),
+                                new XAttribute("Message", strings.GetMessage()),
+                                new XAttribute("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))));
+                            SendMessage(args.Value3.Id, strings.GetStatus(), _token);
                         }
                     }
-                    else
-                    {
-                        if (strings.GetStatus() == "Offline")
-                        {
-                            SendMessage(args.Value3.Id, "already Offline", _token);
-                            return;
-                        }
-                        riders.Add(new XElement("Rider",
-                            new XAttribute("Id", args.Value3.Id),
-                            new XAttribute("Name", args.Value3.Name),
-                            new XAttribute("Status", strings.GetStatus()),
-                            new XAttribute("Message", strings.GetMessage()),
-                            new XAttribute("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))));
-                        SendMessage(args.Value3.Id, strings.GetStatus(), _token);
+                    Console.WriteLine("before save");
+                    doc.SaveToJson(Path.Combine(curPath, "Status.json"));
+                    doc.Save(Path.Combine(curPath, "Status.xml"));
+                    Console.WriteLine("after save");
                     }
-                }
-                Console.WriteLine("before save");
-                doc.SaveToJson(Path.Combine(curPath, "Status.json"));
-                doc.Save(Path.Combine(curPath, "Status.xml"));
-                Console.WriteLine("after save");
-            };
-            stream.StartStream();
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                };
+                stream.StartStream();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         private static IMessage createNewMessage(long? receiverId, string message)
@@ -106,9 +117,10 @@ namespace TwitterStreamClient
 
         private static void SendMessage(long? receiverId, string message, IToken token)
         {
+            Console.WriteLine("before send message");
             IMessage msg = createNewMessage(receiverId, message);
             msg.Publish(token);
-            Console.WriteLine("send message");
+            Console.WriteLine("after send message");
         }
     }
 
