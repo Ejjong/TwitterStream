07b994fc0da066a84294fda86ad075bad49169e1
Jongho Lee <ejjong7@gmail.com>
2014-02-27 11:28:25 +0900

    change tweeinavy => tweetsharp

--- src/TwitterStreamClient/TwitterStreamClient.cs
+++ src/TwitterStreamClient/TwitterStreamClient.cs
@@ -1,59 +1,90 @@
-﻿using System;
+﻿using Mindscape.Raygun4Net;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using TweetSharp;
 
 namespace TwitterStreamClient
 {
-    //public class TwitterStreamClient
-    //{
-    //    TwitterService twitterService;
-    //    XDocument doc;
+    public class TwitterStreamClient
+    {
+        RaygunClient _raygunClient = new RaygunClient("xmDvb3dv/nA8cfGeaqLV8Q==");
+        TwitterService twitterService;
+        IDasUserRepository dasRepo;
+        readonly string token;
+        readonly string secret;
+        readonly string consumerKey;
+        readonly string consumerSecret;
 
-    //    public TwitterStreamClient(string token, string secret, string consumerKey, string consumerSecret)
-    //    {
-    //        twitterService = new TwitterService(consumerKey, consumerSecret);
-    //        twitterService.AuthenticateWith(token, secret);
+        public TwitterStreamClient(string token, string secret, string consumerKey, string consumerSecret, IDasUserRepository dasRepo)
+        {
+            this.token = token;
+            this.secret = secret;
+            this.consumerKey = consumerKey;
+            this.consumerSecret = consumerSecret;
+            this.dasRepo = dasRepo;
+        }
 
-    //        var xmlText = File.ReadAllText("Status.xml");
-    //        doc = XDocument.Parse(xmlText);
-    //    }
+        void CreateService()
+        {
+            twitterService = new TwitterService(consumerKey, consumerSecret);
+            twitterService.AuthenticateWith(token, secret);
+        }
 
-    //    public void Start()
-    //    {
-    //        twitterService.StreamUser((streamEvent, response) =>
-    //        {
-    //            if (response.StatusCode == 0)
-    //            {
-    //                var message = streamEvent as TwitterUserStreamDirectMessage;
-    //                if (message != null)
-    //                {
-    //                    var riders = doc.Descendants("Riders").FirstOrDefault();
-    //                    if(riders != null)
-    //                    {
-    //                        var rider =  riders.Descendants("Rider").SingleOrDefault(r => r.Attribute("Id").Value == message.DirectMessage.SenderId.ToString());
-    //                        if (rider != null)
-    //                        {
-    //                            rider.SetAttributeValue("Name", message.DirectMessage.Sender.Name);
-    //                            rider.SetAttributeValue("Message", message.DirectMessage.Text);
-    //                            rider.SetAttributeValue("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-    //                        }
-    //                        else
-    //                        {
-    //                            riders.Add(new XElement("Rider",
-    //                                new XAttribute("Id", message.DirectMessage.SenderId),
-    //                                new XAttribute("Name", message.DirectMessage.Sender.Name),
-    //                                new XAttribute("Message", message.DirectMessage.Text),
-    //                                new XAttribute("Date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))));
-    //                        }
-    //                    }
-    //                    doc.Save("Status.xml");
-    //                }
-    //            }
-    //        });
-    //    }
-    //}
+        public void Start()
+        {
+            try
+            {
+                CreateService();
+                OnStart();
+            }
+            catch (Exception e)
+            {
+                twitterService.CancelStreaming();
+                twitterService = null;
+
+                CreateService();
+                OnStart();
+            }
+        }
+
+        internal void OnStart()
+        {
+            var result = twitterService.StreamUser((streamEvent, response) =>
+            {
+                if (response.StatusCode == 0)
+                {
+                    var message = streamEvent as TwitterUserStreamDirectMessage;
+                    if (message != null)
+                    {
+                        var strings = message.Text.Split(',');
+                        if (strings.GetStatus() == "" && message.DirectMessage.SenderId != 229481394) return;
+
+                        try
+                        {
+                            var twitterId = message.DirectMessage.SenderId;
+                            var user = new DasUser
+                            {
+                                TwitterId = (int)message.DirectMessage.SenderId,
+                                Name = message.DirectMessage.Sender.Name,
+                                Status = strings.GetStatus(),
+                                Message = strings.GetMessage()
+                            };
+                            dasRepo.InsertOrUpate(user);
+                            twitterService.SendDirectMessage(new SendDirectMessageOptions() { UserId = message.DirectMessage.SenderId, Text = strings.GetStatus() });
+                        }
+                        catch (Exception e)
+                        {
+                            _raygunClient.Send(e);
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+            });
+        }
+    }
 }
--- src/TwitterStreamClient/TwitterStreamClient2.cs
+++ src/TwitterStreamClient/TwitterStreamClient2.cs
@@ -17,104 +17,98 @@ using ServiceStack.OrmLite;
 
 namespace TwitterStreamClient
 {
-    public class TwitterStreamClient2
-    {    
-        static TimeZoneInfo koreaTZI = TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time");
-        RaygunClient _raygunClient = new RaygunClient("xmDvb3dv/nA8cfGeaqLV8Q==");
-        long masterId = 229481394;
+    //public class TwitterStreamClient2
+    //{    
+    //    long masterId = 229481394;
 
-        readonly string token;
-        readonly string secret;
-        readonly string consumerKey;
-        readonly string consumerSecret;
-        readonly IDasUserRepository dasRepo;
+    //    readonly string token;
+    //    readonly string secret;
+    //    readonly string consumerKey;
+    //    readonly string consumerSecret;
+    //    readonly IDasUserRepository dasRepo;
 
-        public TwitterStreamClient2(string token, string secret, string consumerKey, string consumerSecret, IDasUserRepository dasRepo)
-        {
-            this.token = token;
-            this.secret = secret;
-            this.consumerKey = consumerKey;
-            this.consumerSecret = consumerSecret;
-            this.dasRepo = dasRepo;
-            Console.WriteLine("ctor");
-        }
+    //    public TwitterStreamClient2(string token, string secret, string consumerKey, string consumerSecret, IDasUserRepository dasRepo)
+    //    {
+    //        this.token = token;
+    //        this.secret = secret;
+    //        this.consumerKey = consumerKey;
+    //        this.consumerSecret = consumerSecret;
+    //        this.dasRepo = dasRepo;
+    //        Console.WriteLine("ctor");
+    //    }
 
-        IToken CreateToken(string token, string secret, string consumerKey, string consumerSecret)
-        {
-            return new Token(token, secret, consumerKey, consumerSecret);
-            Console.WriteLine("create token");
-        }
+    //    IToken CreateToken(string token, string secret, string consumerKey, string consumerSecret)
+    //    {
+    //        return new Token(token, secret, consumerKey, consumerSecret);
+    //        Console.WriteLine("create token");
+    //    }
 
-        public void Start()
-        {
-            var token = CreateToken(this.token, this.secret, this.consumerKey, this.consumerSecret);
-            OnStart(token);
-        }
+    //    public void Start()
+    //    {
+    //        var token = CreateToken(this.token, this.secret, this.consumerKey, this.consumerSecret);
+    //        OnStart(token);
+    //    }
 
-        internal void OnStart(IToken _token)
-        {
-            try
-            {
-                var stream = new UserStream(_token);
-                Console.WriteLine("create user stream");
-                stream.MessageReceivedFromX += (sender, args) =>
-                {
-                    if (args == null || args.Value3.Id == null) return;
+    //    internal void OnStart(IToken _token)
+    //    {
+    //        try
+    //        {
+    //            var stream = new UserStream(_token);
+    //            Console.WriteLine("create user stream");
+    //            stream.MessageReceivedFromX += (sender, args) =>
+    //            {
+    //                if (args == null || args.Value3.Id == null) return;
 
-                    var strings = args.Value.Text.Split(',');
-                    if (strings.GetStatus() == "" && args.Value3.Id != masterId) return;
+    //                var strings = args.Value.Text.Split(',');
+    //                if (strings.GetStatus() == "" && args.Value3.Id != masterId) return;
 
-                    try
-                    {
-                        var twitterId = args.Value3.Id;
-                        var user = new DasUser
-                        {
-                            TwitterId = (int)args.Value3.Id,
-                            Name = args.Value3.Name,
-                            Status = strings.GetStatus(),
-                            Message = strings.GetMessage()
-                        };
-                        dasRepo.InsertOrUpate(user);
-                        Console.WriteLine("insert or update user");
-                        SendMessage(args.Value3.Id, strings.GetStatus(), _token);
-                        Console.WriteLine("send message");
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("inner exception");
-                        _raygunClient.Send(e);
-                        Console.WriteLine(e.Message);
-                        Start();
-                    }
-                };
-                stream.StartStream();
-                Console.WriteLine("start stream");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("root exception");
-                _raygunClient.Send(ex);
-                Console.WriteLine(ex.Message);
-                Start();
-            }
-        }
+    //                try
+    //                {
+    //                    var twitterId = args.Value3.Id;
+    //                    var user = new DasUser
+    //                    {
+    //                        TwitterId = (int)args.Value3.Id,
+    //                        Name = args.Value3.Name,
+    //                        Status = strings.GetStatus(),
+    //                        Message = strings.GetMessage()
+    //                    };
+    //                    dasRepo.InsertOrUpate(user);
+    //                    Console.WriteLine("insert or update user");
+    //                    SendMessage(args.Value3.Id, strings.GetStatus(), _token);
+    //                    Console.WriteLine("send message");
+    //                }
+    //                catch (Exception e)
+    //                {
+    //                    Console.WriteLine("inner exception");
+    //                    Console.WriteLine(e.Message);
+    //                    Start();
+    //                }
+    //            };
+    //            stream.StartStream();
+    //            Console.WriteLine("start stream");
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("root exception");
+    //            Console.WriteLine(ex.Message);
+    //            Start();
+    //        }
+    //    }
 
-        private static IMessage CreateNewMessage(long? receiverId, string message)
-        {
-            IUser receiver = new User(receiverId);
-            IMessage msg = new Message(message, receiver);
-            Console.WriteLine("create new message");
+    //    private static IMessage CreateNewMessage(long? receiverId, string message)
+    //    {
+    //        IUser receiver = new User(receiverId);
+    //        IMessage msg = new Message(message, receiver);
 
-            return msg;
-        }
+    //        return msg;
+    //    }
 
-        private static void SendMessage(long? receiverId, string message, IToken token)
-        {
-            IMessage msg = CreateNewMessage(receiverId, message);
-            msg.Publish(token);
-            Console.WriteLine("send message");
-        }
-    }
+    //    private static void SendMessage(long? receiverId, string message, IToken token)
+    //    {
+    //        IMessage msg = CreateNewMessage(receiverId, message);
+    //        msg.Publish(token);
+    //    }
+    //}
 
     internal static class StringArrayExtensions
     {
